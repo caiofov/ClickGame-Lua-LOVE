d7393b19920ec469a552e53115ac9ee8fc2e4fc4
caiofov <cfoviana@gmail.com>
2022-03-19 23:25:58 -0300

    first steps

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load( ... ) --runs imediately when the game loads (setups)
+
+end
+
+function love.update(dt) --game loop
+    
+end
+
+function love.draw( ... ) --draws on the screen (similar to update, but involving graphics)
+    
+end
